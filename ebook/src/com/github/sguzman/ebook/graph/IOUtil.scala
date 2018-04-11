75d2128a7269398c6c64875eccaa7e9681f2036b
Salvador Guzman <guzmansalv@gmail.com>
2018-04-11 10:49:34 -0700

    moved async to ioutil

--- ebook/src/com/github/sguzman/ebook/graph/IOUtil.scala
+++ ebook/src/com/github/sguzman/ebook/graph/IOUtil.scala
@@ -2,9 +2,44 @@ package com.github.sguzman.ebook.graph
 
 import cats.effect.IO
 
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object IOUtil {
+  object Async {
+    val work: ListBuffer[(Future[Unit], Throwable => Unit)] =
+      ListBuffer[(Future[Unit], Throwable => Unit)]()
+
+    def ~ : Async.type = Async
+    def sync(): Unit = {
+      val errors =
+        work.par.map(a => util.Try(Await.result(a._1, Duration.Inf)) match {
+          case Success(_) => Right(())
+          case Failure(e) => Left((a._2, e))
+        })
+
+      errors.filter({
+        case Right(_) => false
+        case Left(_) => true
+      }).map(_.left.get).foreach(a => Future(a._1(a._2)))
+
+      work.clear()
+    }
+
+    def onError(e: Throwable): Unit = {
+      println(e.getMessage)
+      e.printStackTrace()
+    }
+
+    def apply(a: => Unit, handle: Throwable => Unit = onError): Async.type = {
+      work.append((Future(a), handle))
+      Async
+    }
+  }
+
   def putLine(msg: => String): IO[Unit] = IO {
     println(msg)
   }
