70ed9a162c1c4495d8cad83f24b6b73acf42fb07
Salvador Guzman <guzmansalv@gmail.com>
2018-04-11 10:40:38 -0700

    finished async object

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -3,9 +3,44 @@ package com.github.sguzman.ebook.graph
 import com.github.sguzman.ebook.graph.IOUtil.Pure
 import com.github.sguzman.ebook.graph.IOUtil.putLine
 
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Main {
   def main(args: Array[String]): Unit = {
-    Pure {
+    val _ = Pure {
+      object Async {
+        val work: ListBuffer[(Future[Unit], Throwable => Unit)] =
+          ListBuffer[(Future[Unit], Throwable => Unit)]()
+
+        def ~ : Async.type = Async
+        def sync(): Unit = {
+          val errors =
+            work.par.map(a => util.Try(Await.result(a._1, Duration.Inf)) match {
+              case Success(_) => Right(())
+              case Failure(e) => Left((a._2, e))
+          })
+
+          errors.filter({
+            case Right(_) => false
+            case Left(_) => true
+          }).map(_.left.get).foreach(a => Future(a._1(a._2)))
+        }
+
+        def onError(e: Throwable): Unit = {
+          println(e.getMessage)
+          e.printStackTrace()
+        }
+
+        def apply(a: => Unit, handle: Throwable => Unit = onError): Async.type = {
+          work.append((Future(a), handle))
+          Async
+        }
+      }
+    } ~ {
       putLine("done").unsafeRunSync()
     }
   }
