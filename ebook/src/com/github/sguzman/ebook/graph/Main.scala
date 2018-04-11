bdfe58bd4504e83f7cdd0ee3fd8b4a505266c456
Salvador Guzman <guzmansalv@gmail.com>
2018-04-11 09:56:26 -0700

    pure :)

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,33 +1,12 @@
 package com.github.sguzman.ebook.graph
 
-import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
-import net.ruippeixotog.scalascraper.dsl.DSL._
-import net.ruippeixotog.scalascraper.model.Element
-import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{element, elementList}
-import org.apache.commons.lang3.StringUtils
+import com.github.sguzman.ebook.graph.IOUtil.Pure
+import com.github.sguzman.ebook.graph.IOUtil.putLine
 
 object Main {
-  implicit final class DocWrap(doc: Browser#DocumentType) {
-    def map(s: String, a: String = ""): Element = doc.>?>(element(s)) match {
-      case Some(v) => v
-      case None => throw new Exception(s"$s $a")
-    }
-
-    def maybe(s: String) = doc.>?>(element(s))
-
-    def flatMap(s: String): List[Element] = doc.>?>(elementList(s)) match {
-      case Some(v) => v
-      case None => throw new Exception(s)
-    }
-  }
-
-  implicit final class StrWrap(str: String) {
-    def doc = JsoupBrowser().parseString(str)
-    def after(sep: String) = StringUtils.substringAfter(str, sep)
-    def afterLast(sep: String) = StringUtils.substringAfterLast(str, sep)
-  }
-
   def main(args: Array[String]): Unit = {
-    println("done")
+    Pure {
+      putLine("done").unsafeRunSync()
+    }
   }
 }
