9cd85169d99a53486674dfe30635c27fba420594
Salvador Guzman <guzmansalv@gmail.com>
2018-04-11 12:10:29 -0700

    posgresql connector

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,14 +1,17 @@
 package com.github.sguzman.ebook.graph
 
 import com.github.sguzman.ebook.graph.IOUtil.{Async, Sync}
+import slick.jdbc.PostgresProfile.api._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val _ = Sync {
+
+      lazy val db = Database.forURL("jdbc:postgresql://localhost:5432/ebooks", driver = "org.postgresql.Driver", user = "alice", password = "pass")
       Async {
         println("begin")
       } ~ {
-        println("next")
+
       }
     } ~ {
       println("done")
