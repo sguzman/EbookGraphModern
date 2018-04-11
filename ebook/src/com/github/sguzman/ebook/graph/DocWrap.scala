b8f3ad3a1e106b13078f64eef4a620e04fe64550
Salvador Guzman <guzmansalv@gmail.com>
2018-04-11 09:52:35 -0700

    move docwrap to another class

---
+++ ebook/src/com/github/sguzman/ebook/graph/DocWrap.scala
@@ -0,0 +1,22 @@
+package com.github.sguzman.ebook.graph
+
+import net.ruippeixotog.scalascraper.browser.Browser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Element
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{element, elementList}
+
+object DocWrap {
+  implicit final class DocWrap(doc: Browser#DocumentType) {
+    def map(s: String, a: String = ""): Element = doc.>?>(element(s)) match {
+      case Some(v) => v
+      case None => throw new Exception(s"$s $a")
+    }
+
+    def maybe(s: String): Option[Element] = doc.>?>(element(s))
+
+    def flatMap(s: String): List[Element] = doc.>?>(elementList(s)) match {
+      case Some(v) => v
+      case None => throw new Exception(s)
+    }
+  }
+}
