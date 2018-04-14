0f92caf48a5f3932ee563807e8d856b06e391384
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 14:28:05 -0700

    print out results

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,25 +1,23 @@
 package com.github.sguzman.ebook.graph
 
 import com.github.sguzman.ebook.graph.io.Sync
-import com.github.sguzman.ebook.graph.sql.Links
-import com.github.sguzman.ebook.graph.wrap.StrWrap._
 import com.github.sguzman.ebook.graph.wrap.DocWrap._
+import com.github.sguzman.ebook.graph.wrap.StrWrap._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val _ = Sync {
-      val cache = Redis.cache("ebooks", _: String)
       val pages = 1 to 1259
 
-      val links = pages.par.flatMap{a =>
+      val links = pages.par.map{a =>
         val url = s"https://it-eb.com/page/$a/"
-        val doc = cache(url).doc
+        val body = Mem.cache(url)
+        val doc = body.doc
         val links = "article.post > div.post-inner > div.post-content > div.post-header > h2.post-title > a[href]"
-
         doc.flatMap(links).map(_.attr("href"))
       }
 
-      Links.insert(links)
+      links foreach println
     }
   }
 }
