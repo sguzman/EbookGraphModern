56c5c16cc30717d70d1befe70268e4a728db8579
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 11:56:03 -0700

    simple iteration over pages

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,9 +1,25 @@
 package com.github.sguzman.ebook.graph
 
+import com.github.sguzman.ebook.graph.io.Sync
 import com.github.sguzman.ebook.graph.sql.Links
+import com.github.sguzman.ebook.graph.wrap.StrWrap._
+import com.github.sguzman.ebook.graph.wrap.DocWrap._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(Links.linkTable)
+    val _ = Sync {
+      val cache = Redis.cache("ebooks", _: String)
+      val pages = 1 to 1259
+
+      val links = pages.par.flatMap{a =>
+        val url = s"https://it-eb.com/page/$a/"
+        val doc = cache(url).doc
+        val links = "article.post > div.post-inner > div.post-content > div.post-header > h2.post-title > a[href]"
+
+        doc.flatMap(links).map(_.attr("href"))
+      }
+
+      Links.insert(links)
+    }
   }
 }
