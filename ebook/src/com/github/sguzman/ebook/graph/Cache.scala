e2eec1bc76256aa560fdb4e4533df6ae022bd673
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 15:22:42 -0700

    finished in-memory redis cache cache

--- ebook/src/com/github/sguzman/ebook/graph/Cache.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Cache.scala
@@ -7,22 +7,36 @@ import com.redis._
 import com.redis.serialization.Parse.Implicits._
 import scalaj.http.Http
 
+import scala.collection.concurrent.TrieMap
 import scala.util.{Failure, Success}
 
 object Cache {
   private val ns = "ebooks"
 
-  private lazy val redis: RedisClient = identity {
+  private lazy val cachingService: (TrieMap[String, Array[Byte]], RedisClient) = identity {
+    val redis = cachingService._2
     println("Init caching client...")
+    val cache: TrieMap[String, Array[Byte]] = redis.hgetall1[String, Array[Byte]](ns) match {
+      case None =>
+        println("No Http cache found - starting from scratch")
+        TrieMap()
+      case Some(v) =>
+        println(s"Found Http cache with ${v.size} entries")
+        TrieMap[String, Array[Byte]](v.toStream: _*)
+    }
+
     Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      println("Writing cache...")
+      redis.hmset(ns, cache)
+
       println("Closing caching client...")
       if (redis.disconnect)
-        println("Disconnected succesfully")
+        println("Disconnected successfully")
       else
         println("Could not disconnect")
     }))
 
-    new RedisClient("localhost", 6379)
+    (cache, new RedisClient("localhost", 6379))
   }
 
   private def http(url: String): String = util.Try(Http(url).asString) match {
@@ -33,15 +47,18 @@ object Cache {
     }
   }
 
-  def get(url: String, ns: String = ns, client: RedisClient = redis): String =
-    client.get[Array[Byte]](s"$ns:$url") match {
+  private def _get(cache: TrieMap[String, Array[Byte]], key: String, client: RedisClient): String =
+    cache.get(key) match {
       case None =>
-        println(s"Miss Http cache for key $url")
-        val body = http(url)
-        client.set(s"$ns:$url", Brotli.compress(body))
+        println(s"Miss Http cache for key $key")
+        val body = http(key)
+        cache.put(key, Brotli.compress(body))
         body
       case Some(v) =>
-        println(s"Hit Http cache for key $url")
+        println(s"Hit Http cache for key $key")
         Brotli.decompress(v)
     }
+
+  def get(key: String): String =
+    _get(cachingService._1, key, cachingService._2)
 }
