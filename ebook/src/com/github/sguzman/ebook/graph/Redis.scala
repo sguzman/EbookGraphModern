b239893fa8e2854e3dd86c560f6ba7724374d37b
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 13:11:26 -0700

    use io instead of future

--- ebook/src/com/github/sguzman/ebook/graph/Redis.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Redis.scala
@@ -2,12 +2,12 @@ package com.github.sguzman.ebook.graph
 
 import java.net.SocketTimeoutException
 
+import cats.effect.IO
 import com.github.sguzman.brotli.Brotli
 import com.redis.RedisClient
 import com.redis.serialization.Parse.Implicits._
 import scalaj.http.Http
 
-import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object Redis {
@@ -35,7 +35,7 @@ object Redis {
   private def _setIfNew(key: String, body: Array[Byte], redis: RedisClient) =
     if (redis.exists(key))
       println(s"Redis key $key exists already")
-    else synchronized {
+    else {
       println(s"Setting Redis key $key")
       redis.set(key, body)
     }
@@ -43,14 +43,16 @@ object Redis {
   private def setIfNew(ns: String, url: String, body: String, redis: RedisClient = redis): Unit =
     _setIfNew(s"$ns:$url", Brotli.compress(body), redis)
 
-  def cache(ns: String, url: String, redis: RedisClient = redis): (String, Future[Unit]) =
-    redis.get[Array[Byte]](s"$ns:$url") match {
-      case None =>
-        println(s"Missed Http cache for $url")
-        val body = http(url)
-        (body, Future(setIfNew(ns, url, body)))
-      case Some(v) =>
-        println(s"Hit Http cache for $url")
-        (Brotli.decompress(v), Future.unit)
+  def cache(ns: String, url: String, redis: RedisClient = redis) =
+    IO {
+      redis.get[Array[Byte]](s"$ns:$url") match {
+        case None =>
+          println(s"Missed Http cache for $url")
+          val body = http(url)
+          (body, IO(setIfNew(ns, url, body)))
+        case Some(v) =>
+          println(s"Hit Http cache for $url")
+          (Brotli.decompress(v), IO.unit)
+      }
     }
 }
