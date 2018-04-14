a913c12b63eb257cc3608fa5e4ba580aa2fcf8eb
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 10:59:45 -0700

    simple test for table

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,25 +1,9 @@
 package com.github.sguzman.ebook.graph
 
-import slick.jdbc.PostgresProfile.api._
-import com.github.sguzman.ebook.graph.DocWrap._
-import com.github.sguzman.ebook.graph.StrWrap._
+import com.github.sguzman.ebook.graph.sql.Links
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val ns = "ebooks"
-    val cache = Redis.cache(ns, _: String)
-
-    val linkTable = TableQuery[Links]
-    println(linkTable)
-
-    val pages = 1 until 1256
-    val links = pages.par.flatMap{a =>
-      val url = s"https://it-eb.com/page/$a/"
-      val body = cache(url)
-      val doc = body.doc
-
-      doc.flatMap("article.post > div.post-inner > div.post-content > div.post-header > h2.post-title > a[href]")
-        .map(_.attr("href"))
-    }
+    println(Links.linkTable)
   }
 }
