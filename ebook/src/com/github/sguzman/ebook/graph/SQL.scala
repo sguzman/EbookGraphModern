56afcc163b2879144cbdf99a5a54cd9a8f668a0a
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 10:09:58 -0700

    abstract db connect

---
+++ ebook/src/com/github/sguzman/ebook/graph/SQL.scala
@@ -0,0 +1,13 @@
+package com.github.sguzman.ebook.graph
+
+import slick.jdbc.PostgresProfile
+
+object SQL {
+  lazy val db: PostgresProfile.backend.DatabaseDef = identity {
+    Runtime.getRuntime.addShutdownHook(new Thread(() => {
+      println("Closing pg")
+      db.close()
+    }))
+    Database.forURL("jdbc:postgresql://localhost:5432/postgres", driver = "org.postgresql.Driver", user = "alice", password = "pass")
+  }
+}
