b78c8a81ab9c7e8d4253b73b7dfd726908275965
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 19:39:30 -0700

    moved apply logic to ~

--- ebook/src/com/github/sguzman/ebook/graph/io/Sync.scala
+++ ebook/src/com/github/sguzman/ebook/graph/io/Sync.scala
@@ -3,10 +3,8 @@ package com.github.sguzman.ebook.graph.io
 import scala.util.{Failure, Success}
 
 case class Sync[A](a: A) {
-  def ~[B](f: A => B): Sync[B] = Sync(f(a))
-
-  def apply[B](b: => B): Sync[B] = util.Try(a) match {
-    case Success(_) => Sync(b)
+  def ~[B](f: A => B): Sync[B] = util.Try(f(a)) match {
+    case Success(b) => Sync(b)
     case Failure(e) => throw e
   }
 }
