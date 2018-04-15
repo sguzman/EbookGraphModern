80e4ee022a0f7c532a4b2ce79344e50ba049c64d
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 18:13:10 -0700

    pass extra type param

--- ebook/src/com/github/sguzman/ebook/graph/Cache.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Cache.scala
@@ -81,7 +81,7 @@ object Cache {
       private def get(key: String): (Option[String], String) =
         _get(incumbent, key)
 
-      def flatMap[A, B, C](col: ParSeq[A], table: TableLike[C])(body: String => Seq[B])(toUrl: A => String): ParSeq[B]  = {
+      def flatMap[A, B, C](col: ParSeq[A], table: TableLike[C, B])(body: String => Seq[B])(toUrl: A => String): ParSeq[B]  = {
         val results: ParSeq[(Option[String], String)] = col.map(toUrl).map(get)
         val (missing, htmlBody): (ParSeq[Option[String]], ParSeq[String]) = results.unzip
         val newValues: ParSeq[(String, String)] = missing
