fe49f008983c87055b8ba3694fe31478f19e1565
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 17:58:08 -0700

    pass table as param

--- ebook/src/com/github/sguzman/ebook/graph/Main.scala
+++ ebook/src/com/github/sguzman/ebook/graph/Main.scala
@@ -1,6 +1,5 @@
 package com.github.sguzman.ebook.graph
 
-import cats.effect.IO
 import com.github.sguzman.ebook.graph.io.Sync
 import com.github.sguzman.ebook.graph.sql.Links
 import com.github.sguzman.ebook.graph.wrap.DocWrap._
@@ -11,13 +10,11 @@ object Main {
     val _ = Sync {
       val pages = 1 to 623
 
-      val links = Cache.get.flatMap(pages.par) {body =>
+      val links = Cache.get.flatMap(pages.par, Links.table) { body =>
         val doc = body.doc
         val links = "div.thumbnail > a[href]"
         doc.flatMap(links).map(_.attr("href"))
       } (a => s"https://www.foxebook.net/page/$a/?sort=default")
-
-      insertIfAbsent.unsafeRunSync()
     }
   }
 }
