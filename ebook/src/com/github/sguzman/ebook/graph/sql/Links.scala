2de2e70bf7c9d6d634300fff9720808980f0505c
Salvador Guzman <guzmansalv@gmail.com>
2018-04-14 20:01:05 -0700

    unique bears insert burden

--- ebook/src/com/github/sguzman/ebook/graph/sql/Links.scala
+++ ebook/src/com/github/sguzman/ebook/graph/sql/Links.scala
@@ -12,7 +12,7 @@ object Links {
     val name = "links"
     final case class Links(tag: Tag) extends Table[(Long, String)](tag, name) {
       def id = column[Long]("id", O.Unique, O.PrimaryKey, O.AutoInc)
-      def link = column[String]("link", O.Length(200), O.Unique)
+      def link = column[String]("link", O.Length(200))
 
       def * = (id, link)
     }
